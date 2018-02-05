b4896dae5d27cbe91287c6cec09f9f445d3812b3
kryseno <kryseno@gmail.com>
2018-02-04 21:50:30 -0800

    [updated] facebookCreds sample template

--- server/config/facebookCreds.sample.js
+++ server/config/facebookCreds.sample.js
@@ -1,5 +1,6 @@
 module.exports = {
     clientID: "FACEBOOK_APP_ID",
     clientSecret: "FACEBOOK_APP_SECRET",
-    callbackURL: "http://localhost:8000/login/facebook/callback"
+    callbackURL: "http://localhost:8000/login/facebook/callback",
+    profileFields: ["FIELD", "FIELD", "FIELD", "FIELD"]
 }
