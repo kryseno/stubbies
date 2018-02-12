a20b0448591158e2c95b0c73be21b40719533020
oowretep <13woopeter@gmail.com>
2018-02-11 19:38:39 -0800

    pulled from finalDev

---
+++ server/facebookCreds.js
@@ -0,0 +1,7 @@
+module.exports = {
+    clientID: "180201912542782",
+    clientSecret: "551e59076a2362bc06629d11cb728d9d",
+    // callbackURL: “http://localhost:4000/auth/facebook/callback“,
+    callbackURL: "http://dev.michaelahn.solutions/auth/facebook/callback",
+    profileFields: ['id', 'name', 'emails', 'picture.type(large)']
+ };
