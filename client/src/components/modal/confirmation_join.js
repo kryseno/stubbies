fc20cf495fb4db6f82afe520a9cf4f8966b32985
pk316 <pr.khiev@gmail.com>
2018-03-11 18:38:28 -0700

    allow user to redirect to profile page when on join modal

--- client/src/components/modal/confirmation_join.js
+++ client/src/components/modal/confirmation_join.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 import './confirmation_modal.css';
 
@@ -21,14 +22,21 @@ class ConfirmationModalJoin extends Component {
             joinStatus: null,
             statusHeader: null
         }
+
         if (confirmStatus === "success") {
-                status.joinStatus = "You have joined this event! A confirmation email will be sent within the hour. You can check the event you joined in your profile.",
+                status.joinStatus1 = "You have joined this event! A confirmation email will be sent within the hour. You can check the event you joined in your ",
+                status.joinStatus2 = ""
+                status.joinStatus3 = <Link to='/profile'>Profile</Link>
                 status.statusHeader = "Success"
         } else if (confirmStatus === "error1") {
-            status.joinStatus = "Cannot join: You are already registered in this event. Check your profile to view your Joined Events.",
+                status.joinStatus1 = "Cannot join: You are already registered in this event. Check your " 
+                status.joinStatus2 = <Link to='/profile'>Profile</Link>
+                status.joinStatus3 = " to view your Joined Events.",
                 status.statusHeader = "Error"
         } else if (confirmStatus === "error2") {
-                status.joinStatus = "Cannot join this event: The group size has reached its max.",
+                status.joinStatus1 = "Cannot join this event: The group size has reached its max.",
+                status.joinStatus2 = ""
+                status.joinStatus3 = "",
                 status.statusHeader = "Error"
         } else {
             status.joinStatus = "An error occured... Try again later."
@@ -48,7 +56,7 @@ class ConfirmationModalJoin extends Component {
                             <h4 className="modal-title">{status.statusHeader}</h4>
                         </div>
                         <div className="modal-body confirmBody">
-                            <p>{status.joinStatus}</p>
+                            <p>{status.joinStatus1}{status.joinStatus2}{status.joinStatus3}</p>
                         </div>
                         <div className="modal-footer col-sm-12 col-xs-12">
                             <button type="button" className="btn btn-default col-sm-12 col-xs-12" onClick={this.toggleModal}>Close</button>
