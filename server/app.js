0e84a2f16f26b985ab8f8e6f4d46adb0b2802ca1
kryseno <kryseno@gmail.com>
2018-03-13 13:29:50 -0700

    [updated] cleaned up code

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const morgan_common = ":remote-addr - :remote-user [:date[web]] ':method :url HTTP/:http-version' :status :res[content-length]";
-const fs = require('fs');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -23,14 +22,11 @@ app.use( express.json() );
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
 //Morgan: error logger middleware
-app.use(morgan('dev'));
-
-// app.use(morgan(morgan_common, {
-//     stream: fs.createWriteStream(path.join(__dirname, 'errorLogs', 'serverError.log'), {flags: 'r+'}),
-//     skip: function(req, res){
-//         return res.statusCode < 400
-//     }
-// }));
+app.use(morgan(morgan_common, {
+    skip: function(req, res){
+        return res.statusCode < 400
+    }
+}));
 
 //Session
 app.use(session({
@@ -49,17 +45,6 @@ require('./auth')(app, passport);
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
-
-// app.use('*', function(err, req, res, next){
-//     console.log('There was an error');
-//         let errorData= {Date: new Date().toLocaleString(),errorMessage: err.stack};
-//         fs.appendFile(path.join(__dirname, 'errorLogs', 'serverError.log'), JSON.stringify(errorData) + '\n', function (err) {
-//             if (err) next(err); 
-//             console.log('Updated!');
-//          });
-//         res.status(500).render('error', { error: err });
-//         next(err);
-// });
 
 // Listen
 app.listen(PORT, function(){
