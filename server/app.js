8abb1dcf21924d92ebe43d85ac068ec7252c08e8
kryseno <kryseno@gmail.com>
2018-03-14 02:38:47 -0700

    [removed] api.js d/t routes dir being used in place

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(morgan(morgan_common, {
 
 //Session
 app.use(session({
-    secret: 'ssshhhh',
+    secret: 'session',
     resave: true,
     saveUninitialized: true
 }));
@@ -42,7 +42,6 @@ require('./config/passport')(passport);
 require('./routes/join')(app, passport);
 require('./routes/create')(app, passport);
 require('./routes/profile')(app, passport);
-// require('./api')(app, passport);
 require('./auth')(app, passport);
 
 app.get('*', function(req, res) {
